567c3d2d54c683995fe06c861660e613f8b89cea
crdenn <cr.denn@gmail.com>
2014-05-19 10:21:21 -0400

    Blog update at 2014-05-19 14:21:21

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,3 +16,18 @@
     });
 
 }(jQuery));
+
+/* Buster: correct local directory links to not include "index.html" */
+(function($){
+    var absUrlPattern = new RegExp('^(?:[a-z]+:)?//', 'i');
+    $(document).ready(function(){
+        $('a').each(function(){
+            var e = $(this);
+            var href = e.attr('href');
+            if (!absUrlPattern.test(href)) {
+                var new_href = href.replace(/\/index\.html$/, '/');
+                e.attr('href', new_href);
+            }
+        });
+    });
+}(jQuery));
